89d2791bebaceb2838349f77b78cf3989919d157
Christopher Eason <ceason5002@gmail.com>
2025-09-30 11:38:33 -0500

    updated book to be dependent on media

--- book.rb
+++ book.rb
@@ -1,13 +1,11 @@
-class Book
-  attr_reader :title, :author, :pages
+require_relative "media"
+
+class Book < Media
+  attr_reader :pages
 
   def initialize(title, author, pages)
-    raise ArgumentError, "Title must be a non-empty string" unless title.is_a?(String) && !title.empty?
-    raise ArgumentError, "Author must be a non-empty string" unless author.is_a?(String) && !author.empty?
+    super(title, author)
     raise ArgumentError, "Pages must be a positive integer" unless pages.is_a?(Integer) && pages > 0
-
-    @title = title
-    @author = author
     @pages = pages
   end
 
