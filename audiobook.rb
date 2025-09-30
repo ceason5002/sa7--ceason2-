0bcd72f234edcae56204a074fbf82c606c2012d3
Christopher Eason <ceason5002@gmail.com>
2025-09-30 11:39:19 -0500

    updated audiobook to be dependent on media

--- audiobook.rb
+++ audiobook.rb
@@ -1,13 +1,11 @@
-class Audiobook
-  attr_reader :title, :author, :duration
+require_relative "media"
+
+class Audiobook < Media
+  attr_reader :duration
 
   def initialize(title, author, duration)
-    raise ArgumentError, "Title must be a non-empty string" unless title.is_a?(String) && !title.empty?
-    raise ArgumentError, "Author must be a non-empty string" unless author.is_a?(String) && !author.empty?
+    super(title, author)
     raise ArgumentError, "Duration must be a positive number" unless duration.is_a?(Numeric) && duration > 0
-
-    @title = title
-    @author = author
     @duration = duration.to_f
   end
 
