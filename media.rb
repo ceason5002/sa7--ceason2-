70404d33de812e16a3c1bc3f04bf1907ad1c6f4a
Christopher Eason <ceason5002@gmail.com>
2025-09-30 13:31:08 -0500

    updated media.rb

--- media.rb
+++ media.rb
@@ -1,12 +1,12 @@
 class Media
-    attr_reader :title, :author
+  attr_reader :title, :author, :progress
 
-    def initialize(title, author)
-        raise ArgumentError, "Title must be a non-empty string" unless title.is_a?(String) && !title.empty?
-        raise ArgumentError, "Author must be a non-empty string" unless author.is_a?(String) && !author.empty?
+  def initialize(title, author)
+    raise ArgumentError, "Title must be a non-empty string" unless title.is_a?(String) && !title.empty?
+    raise ArgumentError, "Author must be a non-empty string" unless author.is_a?(String) && !author.empty?
 
-        @title = title
-        @author = author
- 
-    end
+    @title = title
+    @author = author
+    @progress = 0
+  end
 end
